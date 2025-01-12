89a6ee30a9f6b2d5337f013b0039cce6a7f7c8b2
adi07-04 <68350643+adi07-04@users.noreply.github.com>
2025-01-12 20:52:22 +0530

    first draft of web app

---
+++ Server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes to serve HTML files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
